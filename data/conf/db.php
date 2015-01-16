be32c75bd2e42becc775a7512bba1b73d800767d
liqineng@126.com <liqineng@126.com>
2015-01-17 03:36:10 +0800

    ok

---
+++ data/conf/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'thinkcmf',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'root',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'sp_lqn',
+    //密钥
+    "AUTHCODE" => '7H0PVfM1nOCYrTF3af',
+    //cookies
+    "COOKIE_PREFIX" => 'bZCjEX_',
+);
+?>
